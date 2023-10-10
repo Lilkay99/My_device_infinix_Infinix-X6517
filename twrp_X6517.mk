3f237f0b23dbbcda195be12e1caa9c04291d1215
Lilkay99 <117673933+Lilkay99@users.noreply.github.com>
2023-10-10 10:05:57 +0100

    Update and rename omni_Infinix-X6517.mk to twrp_X6517.mk

--- twrp_X6517.mk
+++ twrp_X6517.mk
@@ -9,14 +9,14 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+# Inherit some common twrp stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
 
-# Inherit from Infinix-X6517 device
-$(call inherit-product, device/infinix/Infinix-X6517/device.mk)
+# Inherit from X6517 device
+$(call inherit-product, device/infinix/X6517/device.mk)
 
-PRODUCT_DEVICE := Infinix-X6517
-PRODUCT_NAME := omni_Infinix-X6517
+PRODUCT_DEVICE := X6517
+PRODUCT_NAME := twrp_X6517
 PRODUCT_BRAND := Infinix
 PRODUCT_MODEL := Infinix X6517
 PRODUCT_MANUFACTURER := infinix
