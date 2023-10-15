5cd82476469f3f56e60aa30fe417498b371add92
Lilkay99 <117673933+Lilkay99@users.noreply.github.com>
2023-10-15 19:33:04 +0100

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo twrp_X6517-user
-add_lunch_combo twrp_X6517-userdebug
-add_lunch_combo twrp_X6517-eng
+COMMON_LUNCH_CHOICES twrp_X6517-user
+COMMON_LUNCH_CHOICES twrp_X6517-userdebug
+COMMON_LUNCH_CHOICES twrp_X6517-eng
